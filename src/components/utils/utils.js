65dff3b2ba09f7cfc15f361d5796d7c0cadc3f46
Marco Persico <mpersico@gmail.com>
2019-08-08 19:29:32 -0300

    compare method on utils fix

--- src/components/utils/utils.js
+++ src/components/utils/utils.js
@@ -7,14 +7,19 @@ class Utils {
    * This method is to compare two unsorted arrays returns true
    * when the array are equal otherwise returns false
    */
-  static compare(arr1, arr2) {
+  compare(arr1, arr2) {
+    let isFalse;
     if (arr1.length === arr2.length) {
       arr1.forEach(elemArr1 => {
         if (!arr2.includes(elemArr1)) {
-          return false;
+          isFalse = true;
         }
       });
-      return true;
+      if (isFalse) {
+        return false
+      } else {
+        return true;
+      }
     }
     return false;
   }
